deac6b10f49a88aa3cdd25127575b1400ae43955
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-06-07 21:50:41 +1000

    added user routes and connected mongo db

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+const UserModel = require('../models/UserModel')
+
+router.post('/', async (req, res) => {
+    try {
+        const { email, password, passwordVerify } = req.body
+
+        // validation
+
+        if (!email || !password || !passwordVerify) {
+            return res.status(400).json({ errorMessage: 'Please enter all required fields.' })
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ errorMessage: 'Please enter a password of at least 6 characters.' })
+        }
+
+        if (password !== passwordVerify) {
+            return res.status(400).json({ errorMessage: 'Please enter the same password twice.' })
+        }
+
+        const existingUser = await UserModel.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({ errorMessage: 'An account with this email already exists.' })
+        }
+
+    } catch (e) {
+        console.error(e)
+        res.status(500).send()
+    }
+})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,14 @@
+require('dotenv').config()
 const express = require('express')
-const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.use(express.json())
+app.use('/auth', require('./routers/userRouter'))
 
-const users = []
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING).catch(e => console.log(e))
 
-app.get('/users', (req, res) => {
-    res.json(users)
+app.listen(4000, () => {
+    console.log('Running on 4000')
 })
-
-app.post('/users', async (req, res) => {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    const user = {
-        username: req.body.username,
-        password: hashedPassword
-    }
-    users.push(user)
-    res.status(201).send()
-})
-
-app.post('/login', async (req, res) => {
-    const user = users.find(x => x.username === req.body.username)
-    if (!user) {
-        res.send('No user found')
-    } else {
-        try {
-            if (await bcrypt.compare(req.body.password, user.password)) {
-                res.send('Success')
-            } else {
-                res.send('Invalid password')
-            }
-        } catch (e) {
-            res.send(e)
-        }
-    }
-
-})
-
-app.listen(4000)
