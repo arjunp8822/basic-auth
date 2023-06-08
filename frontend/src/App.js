8c8ff5d28083ecfe0c18100d7dc8f6b9eb7eeb23
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-06-09 09:51:52 +1000

    added routing to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import { useState } from 'react';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Recipes from "./pages/Recipes";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <div className="App">
-      Test
-    </div>
+    <>
+      <Navbar />
+      <Routes className="App">
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/recipes" element={<Recipes />} />
+      </Routes>
+    </>
   );
 }
 
